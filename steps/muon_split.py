6f122039f97ddfad129b941b822e2a554f1fc60f
pascalgutjahr <pascal.gutjahr@tu-dortmund.de>
2020-05-20 12:12:47 -0500

    add tree pos dir

--- steps/muon_split.py
+++ steps/muon_split.py
@@ -4,16 +4,13 @@
 def muon_splitter(tray, name):
     from icecube import dataclasses, icetray
 
-    def dir_pos(frame):
+    def dir_pos_tree(frame):
         frame.Put('NewDirection', dataclasses.I3Direction())
         frame.Put('NewPosition', dataclasses.I3Position())		
-    
-    def new_tree(frame):
         frame.Put('NewTree', dataclasses.I3MCTree())
 
 
-    tray.AddModule(dir_pos, 'addDirPos', Streams=[icetray.I3Frame.DAQ])
-    tray.AddModule(new_tree, 'addTree', Streams=[icetray.I3Frame.DAQ])
+    tray.AddModule(dir_pos_tree, 'addDirPos', Streams=[icetray.I3Frame.DAQ])
 	
 
    
