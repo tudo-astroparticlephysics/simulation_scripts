0aaa1306faaf24498e253e06b6d6e9b470c381a4
mhuen <mhuen@users.noreply.github.com>
2022-08-16 12:10:04 +0200

    CORSIKA first testrun: fix minor things

--- steps/step_0_corsika_py3_v4_1_0_branch_TrimShower.py
+++ steps/step_0_corsika_py3_v4_1_0_branch_TrimShower.py
@@ -85,7 +85,8 @@ def main(cfg, run_number, scratch):
     # Run script to generate CORSIKA file
     # -----------------------------------
     corsika_file = outfile + '.corsika_temp.i3'
-    module = load_class(cfg['corsika_module'])
+    module_class = load_class(cfg['corsika_module'])
+    module = module_class()
 
     default_settings = {
 
