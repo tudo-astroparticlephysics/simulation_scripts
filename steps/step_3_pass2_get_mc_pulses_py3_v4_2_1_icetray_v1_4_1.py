b57051fc00e054e312ee83692dde939ed601b978
Mirco Huennefeld <mirco.huennefeld@tu-dortmund.de>
2022-11-02 15:28:26 +0100

    Feature: BFRv2 support (#9)
    * BFRv2 benchmark cascades
    
    * update snowstorm propagation step to icectray v1.4.1
    
    * update snowstorm propagation step to icectray v1.4.1: work-around for detector sim: use older combo version and py3-v4.1.0 due to some GIL issue

---
+++ steps/step_3_pass2_get_mc_pulses_py3_v4_2_1_icetray_v1_4_1.py
@@ -0,0 +1,6 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.2.1/icetray-start
+#METAPROJECT icetray/v1.4.1
+from step_3_pass2_get_mc_pulses import *
+
+if __name__ == '__main__':
+    main()
