23ed696c9345f61c3e003ebafd00f8f07f599b44
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2023-01-18 17:18:28 +0100

    updated to new icetray

---
+++ steps/step_3_pass2_L1_py3_v4_2_1.py
@@ -0,0 +1,6 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.2.1/icetray-start
+#METAPROJECT /data/user/lneste/i3/icetray/build
+from step_3_pass2_L1_py3_v4_1_0 import *
+
+if __name__ == '__main__':
+    main()
