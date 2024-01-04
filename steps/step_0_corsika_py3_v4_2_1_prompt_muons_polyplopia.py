48f0c8a1f723ba3f61d679be05088d494efeefb0
pascalgutjahr <pascal.gutjahr@tu-dortmund.de>
2024-01-04 09:16:08 -0600

    clean up: move configs to dnn_selections, remove test steps

--- steps/step_0_corsika_py3_v4_2_1_prompt_muons_polyplopia.py
+++
@@ -1,6 +0,0 @@
-#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.2.1/icetray-start
-#METAPROJECT /data/user/pgutjahr/software//icetray/v1.5.0_polyplopia/build
-from step_0_corsika_py3_v4_1_0_branch_TrimShower import *
-
-if __name__ == '__main__':
-        main()
