949d753277112ffa28c3d4621bb35ebdc22b158c
mhuen <mhuen@users.noreply.github.com>
2024-02-08 14:00:15 +0100

    Update to latest official release

---
+++ steps/step_2_snowstorm_detector_simulation_py3_v4_3_0_icetray_v1_9_1.py
@@ -0,0 +1,6 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.3.0/icetray-start
+#METAPROJECT icetray/v1.9.1
+from step_2_snowstorm_detector_simulation import *
+
+if __name__ == '__main__':
+    main()
--- steps/step_4_pass2_L2_py3_v4_2_1.py
+++
@@ -1,6 +0,0 @@
-#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.2.1/icetray-start
-#METAPROJECT /data/user/lneste/i3/icetray/build
-from step_4_pass2_L2_py3_v4_1_0 import *
-
-if __name__ == '__main__':
-    main()
