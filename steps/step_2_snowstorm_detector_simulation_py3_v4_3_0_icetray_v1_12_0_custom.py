38525dd4185da072d8a20e5b15bb9e65e5bf9bf1
mhuen <mhuen@users.noreply.github.com>
2024-12-09 20:54:19 -0600

    use custom build

---
+++ steps/step_2_snowstorm_detector_simulation_py3_v4_3_0_icetray_v1_12_0_custom.py
@@ -0,0 +1,7 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py3-v4.3.0/icetray-start
+#METAPROJECT /data/user/mhuennefeld/software/icecube/py3-v4.3.0/v1.12.0__propagate_names/build
+
+from step_2_snowstorm_detector_simulation import *
+
+if __name__ == '__main__':
+    main()
